37503654b507360208e70b33b6373f3273c6e9a8
Edgar Montano <em2532@nyu.edu>
2018-03-09 14:24:29 -0500

    part 2.1: created generic object class to store various objects such as player, npc, item, etc

--- main.py
+++ main.py
@@ -21,21 +21,66 @@ FULLSCREEN = False  #fullscreen on startup
 TITLE = 'PyRL'
 ##############################################
 
+#############CLASS DEFINITIONS################
+class Object:
+    """
+    This is a generic object that can represent the 
+    player, an npc, item, stairs, etc.
+    """
+    def __init__(self,x,y,char,color):
+        """
+        Constructor that determines location of object
+        character represent object, and the color of the object.
+        This constructor should always be called on creating a new 
+        object.
+        """
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+    def move(self, dx, dy):
+        """
+        Moves the object based on a predefined input:
+        negative values denote for a change in opposite direction.
+        """
+        self.x += dx
+        self.y += dy 
+    def draw(self):
+        """
+        Sets the color and then draws the character represented by the positional values.
+        """
+        libtcod.console_set_default_foreground(con,self.color)
+        libtcod.console_put_char(con,self.x,self.y,self.char,libtcod.BKGND_NONE)
+    def clear(self):
+        """
+        Erases the character
+        """
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+##############################################
+
 
 ###############INITIALIZATION################
 # set custom greyscale font
-libtcod.console_set_custom_font('arial10x10.png',libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_set_custom_font(
+    'arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
 #now initialization the console window, setting
-libtcod.console_init_root(SCREEN_WIDTH,SCREEN_HEIGHT,TITLE,FULLSCREEN)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE, FULLSCREEN)
 
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
+# creates a console buffer we can use to store results of drawing functions
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# Initializes the player object, an npc object, then creates 
+# a list of all objects to iterate through
+# makes updating all objects easier. 
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc    = Object(SCREEN_WIDTH/2-5, SCREEN_HEIGHT/2, 'd',libtcod.yellow)
+objects = [npc,player]
 ##############################################
 
 ###############FUNCTIONS######################
 def handle_keys():
-    global playerx, playery
+    #global playerx, playery
     #blocks the game and awaits input
     key = libtcod.console_wait_for_keypress(True)
     if key.vk == libtcod.KEY_ENTER and key.lalt:
@@ -43,31 +88,36 @@ def handle_keys():
     elif key.vk == libtcod.KEY_ESCAPE:
         return True
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        playery -= 1
+        player.move(0,-1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        playery += 1
+        player.move(0,1)
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        playerx -= 1
+        player.move(-1,0)
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        playerx += 1
+        player.move(1,0)
 ##############################################
 
 
 # Main game loop
 while not libtcod.console_is_window_closed():
     
-    #set foreground of player to white
-    libtcod.console_set_default_foreground(0,libtcod.white)
-
-    #place character at coordinates (1,1)
-    libtcod.console_put_char(0,playerx,playery,'@',libtcod.BKGND_NONE)
+    # This iterates through each object and 
+    # draws them onto the screen, instead of manually 
+    # calling console_put_char
+    for object in objects:
+        object.draw()
+    
+    # blits the contents of the new console to the root
+    libtcod.console_blit(con,0,0, SCREEN_WIDTH,SCREEN_HEIGHT,0,0,0)
 
     #flushes changes to screen
     libtcod.console_flush()
 
-    # place an empty space in players previous place in order to avoid 
-    # printing a trail of characters on screen
-    libtcod.console_put_char(0, playerx, playery,' ', libtcod.BKGND_NONE)
+    # Iterates through all objects, and clears them from the screen
+    # this is so that next turn, we dont have trails of previous moves 
+    # still on the screen. 
+    for object in objects:
+        object.clear()
 
     #determine whether exit has been called, otherwise move character
     #the reason we call handle keys last is because it initiates the start of the next turn
